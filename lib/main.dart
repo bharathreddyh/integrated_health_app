b1d8ccd03077604026d8a6b8e9923c303459be33
bharathreddyh <bharathreddy@gmail.com>
2026-09-25 01:04:29 +0530

    Force landscape-only orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 // lib/main.dart
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'screens/auth/login_screen.dart';
@@ -29,6 +30,10 @@ final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
+  ]);
   print('App starting...');
 
   // Initialize Firebase
